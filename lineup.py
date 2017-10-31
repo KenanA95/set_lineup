e86ce28992be413802cf617e1964c6d8209d5590
Kenan <kenan.r.alkiek@gmail.com>
2017-10-31 00:13:33 -0400

    Initial commit

---
+++ lineup.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import json
+import time
+
+# Grab my login info
+with open('credentials.json') as json_data:
+    yahoo = json.load(json_data)
+
+login_url = "https://login.yahoo.com/config/login?.src=fantasy&specId=usernameRegWithName&." \
+             "intl=us&.lang=en-US&.done=https://basketball.fantasysports.yahoo.com/"
+
+driver = webdriver.Chrome()
+driver.get(login_url)
+
+# Put my email in and hit next
+driver.find_element_by_id("login-username").send_keys(yahoo['user'])
+driver.find_element_by_id("login-signin").click()
+
+# Give it a second to complete the redirect
+time.sleep(1)
+
+# Put my password in and login
+driver.find_element_by_id("login-passwd").send_keys(yahoo['password'])
+driver.find_element_by_id("login-signin").click()
+
+# Start active players for today's date
+date = time.strftime("%Y-%m-%d")
+active_players = "https://basketball.fantasysports.yahoo.com/nba/71943/2?date={0}".format(date)
+driver.get(active_players)
+driver.close()
